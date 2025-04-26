96e1fcee2ce7f29633b69e37a75710ca67eb4dfb
Minseop Ji <jiminsub12@naver.com>
2025-04-27 08:04:09 +0900

    Update web_crawling.py

--- pages/web_crawling.py
+++ pages/web_crawling.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 def main():
-    api_key = st.secrets["API_KEY"]
+    api_key = st.secrets["OPENAI_API_KEY"]
     client = openai.OpenAI(api_key = api_key)
     candidates = ["A", "B", "C", "D", "E"]
 
